7ee1fa0586c9303552a2028ce27fb2555fbc6a17
Tatyana Balaschova <t.balaschova@mail.ru>
2017-06-05 21:46:42 +0600

    Redirect to books list for authenticated users

--- src/stroustrup/views.py
+++ src/stroustrup/views.py
@@ -1,8 +1,15 @@
 from django.views import generic
+from django.shortcuts import redirect
 
 
 class HomePage(generic.TemplateView):
     template_name = "home.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect('books:list')
+
+        return super(HomePage, self).dispatch(request, *args, **kwargs)
 
 
 class AboutPage(generic.TemplateView):
